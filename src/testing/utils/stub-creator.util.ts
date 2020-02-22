90a6d9c539a6dfceba5f04984ffbe66861d792e6
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-22 17:04:47 +0000

    feat(testing): Add fake method to stub creator

--- src/testing/utils/stub-creator.util.ts
+++ src/testing/utils/stub-creator.util.ts
@@ -2,4 +2,8 @@ export class StubCreator {
   static create<T>(stubbedClass: any): T {
     return (new stubbedClass() as Partial<T>) as T;
   }
+
+  static fake<T>(stubbedClass: any): T {
+    return (stubbedClass as Partial<T>) as T;
+  }
 }
