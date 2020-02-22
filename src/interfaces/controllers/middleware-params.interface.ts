8dad1aa137c96c49f98e26d91556219d105e17d3
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-22 15:45:37 +0000

    fix(middleware-params): Remove unused import

--- src/interfaces/controllers/middleware-params.interface.ts
+++ src/interfaces/controllers/middleware-params.interface.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 import { IBaseUser } from './base-user.interface';
-import express from 'express';
 
 export interface IReq<T extends IBaseUser = IBaseUser> extends Request {
   user: T;
