7e73e5b79868cdf0db0f5776dc51645700f60f40
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-23 17:40:28 +0000

    feat(testing): Export express router stub

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -1,4 +1,5 @@
 import { StubCreator } from './utils/stub-creator.util';
 import { ExpressAppStub } from './stubs/express-app.stub';
+import { ExpressRouterStub } from './stubs/express-router.stub';
 
-export { StubCreator, ExpressAppStub };
+export { StubCreator, ExpressAppStub, ExpressRouterStub };
