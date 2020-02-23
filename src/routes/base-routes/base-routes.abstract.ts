090e5f7f94b04d6f5e50b16a5d846804acae2e40
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-23 20:45:59 +0000

    feat(base-routes): Add controller field in base routes

--- src/routes/base-routes/base-routes.abstract.ts
+++ src/routes/base-routes/base-routes.abstract.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
-export abstract class BaseRoutes {
+export abstract class BaseRoutes<T> {
   protected router: Router;
+  protected controller: T;
 
-  constructor(router: Router) {
+  constructor(router: Router, controller: T) {
     this.router = router;
+    this.controller = controller;
   }
 
   public get routes(): Router {
