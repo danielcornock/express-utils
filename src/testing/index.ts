7a8634ac35eb969651976722bb7b387a4e3af23c
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-23 22:30:25 +0000

    fix(testing): Remove next stub from exports

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -1,6 +1,6 @@
 import { StubCreator } from './utils/stub-creator.util';
 import { ExpressAppStub } from './stubs/express-app.stub';
 import { ExpressRouterStub } from './stubs/express-router.stub';
-import { ReqStub, ResStub, nextStub } from './stubs/middleware-params.stub';
+import { ReqStub, ResStub } from './stubs/middleware-params.stub';
 
-export { StubCreator, ExpressAppStub, ExpressRouterStub, ReqStub, nextStub, ResStub };
+export { StubCreator, ExpressAppStub, ExpressRouterStub, ReqStub, ResStub };
