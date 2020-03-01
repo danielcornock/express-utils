c5e8062d488c39ef571c642a366b545ab69645b4
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-03-01 21:02:45 +0000

    add logger and export internal server error exception

--- src/http/errors/index.ts
+++ src/http/errors/index.ts
@@ -1,3 +1,4 @@
 export * from './http-exception';
 export * from './not-found-exception';
 export * from './unauthorised-exception';
+export * from './internal-server-error-exception';
---
+++ src/utilities/logger.ts
@@ -0,0 +1,5 @@
+export class Logger {
+  static console(msg: string): void {
+    console.log(msg);
+  }
+}
