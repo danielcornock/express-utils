d91774e0ade0f4c90f330aef5f827dbee331a7c5
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-03-02 00:07:26 +0000

    add json to res stub

--- src/testing/stubs/middleware-params.stub.ts
+++ src/testing/stubs/middleware-params.stub.ts
@@ -1,6 +1,7 @@
 export class ResStub {
   send: jest.Mock = jest.fn();
   status: jest.Mock = jest.fn().mockReturnValue({ json: jest.fn() });
+  json: jest.Mock = jest.fn();
 }
 
 export class ReqStub {
