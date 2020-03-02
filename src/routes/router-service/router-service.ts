2b030399312b6200656e3664f0fd1123aacc1d67
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-03-02 23:01:09 +0000

    add missing methods to router service

--- src/routes/router-service/router-service.ts
+++ src/routes/router-service/router-service.ts
@@ -11,6 +11,22 @@ export class RouterService {
     this.router.get(params, this._tryCatch(fn));
   }
 
+  public post(params: string, fn: Function) {
+    this.router.post(params, this._tryCatch(fn));
+  }
+
+  public put(params: string, fn: Function) {
+    this.router.put(params, this._tryCatch(fn));
+  }
+
+  public delete(params: string, fn: Function) {
+    this.router.delete(params, this._tryCatch(fn));
+  }
+
+  public use(...args: any) {
+    this.router.use(...args);
+  }
+
   private _tryCatch(fn: Function) {
     return (...args: any) => {
       fn(...args).catch(args[2]);
